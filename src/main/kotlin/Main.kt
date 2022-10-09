9e9119dc2ff9cec334ceeb2b1e61eae1fa80f912
Dallon Feldner <dallon.feldner@gmai.com>
2022-10-09 08:55:27 -0500

    Basic file watch

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,8 @@
 import com.dallonf.ktcause.*
 import kotlinx.cli.*
+import java.nio.file.FileSystems
 import java.nio.file.Paths
+import java.nio.file.StandardWatchEventKinds
 import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
@@ -16,20 +18,42 @@ fun main(args: Array<String>) {
     val rootDirPath = rootDirectory?.let {
         Paths.get(it)
     } ?: Paths.get("")
-    val codeLoader = CodeLoader(rootDirPath)
-    val filepath = run {
-        val paddedDay = day.toString().padStart(2, '0')
-        "project/puzzles/day${paddedDay}.cau"
+
+    while (true) {
+        val codeLoader = CodeLoader(rootDirPath)
+        val filepath = run {
+            val paddedDay = day.toString().padStart(2, '0')
+            "project/puzzles/day${paddedDay}.cau"
+        }
+        codeLoader.addFileWithDependencies(filepath)
+
+        val runner = Runner(LangVm(codeLoader.builder.build()))
+
+        for (part in parts) {
+            println()
+            println("${part}:")
+            runner.run(filepath, part)
+        }
+
+        val watchService = FileSystems.getDefault().newWatchService()
+        val filesToWatch = runner.vm.codeBundle.files.keys.mapNotNull { file ->
+            if (file.startsWith("core/")) {
+                return@mapNotNull null
+            }
+            rootDirPath.resolve(file)
+        }
+        val pathsToWatch = filesToWatch.map { it.parent }.toSet()
+        val watchKeys = pathsToWatch.associateWith { dir ->
+            dir.register(
+                watchService,
+                StandardWatchEventKinds.ENTRY_MODIFY,
+                StandardWatchEventKinds.ENTRY_DELETE
+            )
+        }
+
+        watchService.take()
+        for (watchKey in watchKeys.values) {
+            watchKey.cancel()
+        }
     }
-    codeLoader.addFileWithDependencies(filepath)
-
-    val runner = Runner(LangVm(codeLoader.builder.build()))
-
-    for (part in parts) {
-        println()
-        println("${part}:")
-        runner.run(filepath, part)
-    }
-
-    exitProcess(0)
 }
