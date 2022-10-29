b4eb20501369b24c3914a2ccf5ee6e59a13806f9
Dallon Feldner <dallon.feldner@gmail.com>
2022-10-29 06:12:33 -0500

    Setup rendering for Day 1

--- src/main/kotlin/Runner.kt
+++ src/main/kotlin/Runner.kt
@@ -9,93 +9,101 @@ import java.nio.file.Path
 class Runner(val vm: LangVm, val rootDir: Path, val window: VizWindow) {
     fun run(file: String, part: String) {
         val startTime = System.nanoTime()
-        var executionState = vm.executeFunction(file, part, listOf())
+        val returnVal = try {
+            var executionState = vm.executeFunction(file, part, listOf())
 
-        fun getInputFile(path: String) = rootDir.resolve("project/puzzles").resolve(path).toFile()
+            fun getInputFile(path: String) = rootDir.resolve("project/puzzles").resolve(path).toFile()
 
-        var drawQueue = mutableListOf<RuntimeValue.RuntimeObject>()
+            var drawQueue = mutableListOf<RuntimeValue.RuntimeObject>()
 
-        while (executionState is RunResult.Caused) {
-            val signal = executionState.signal
+            while (executionState is RunResult.Caused) {
+                val signal = executionState.signal
 
-            val resultValue = when (signal.typeDescriptor.id) {
-                vm.codeBundle.getBuiltinTypeId("Debug") -> {
-                    val value = signal.values[0]
-                    if (value is RuntimeValue.Text) {
-                        println(value.value)
-                    } else {
-                        println(value.debug())
+                val resultValue = when (signal.typeDescriptor.id) {
+                    vm.codeBundle.getBuiltinTypeId("Debug") -> {
+                        val value = signal.values[0]
+                        if (value is RuntimeValue.Text) {
+                            println(value.value)
+                        } else {
+                            println(value.debug())
+                        }
+                        RuntimeValue.Action
                     }
-                    RuntimeValue.Action
+
+                    vm.codeBundle.getBuiltinTypeId("AssumptionBroken") -> {
+                        val value = signal.values[0] as RuntimeValue.Text
+                        println(
+                            Ansi.ansi().fg(Ansi.Color.RED).render(vm.getExecutionTrace())
+                                .render("Assumption broken: " + value.value).reset()
+                        )
+                        break
+                    }
+
+                    vm.codeBundle.getTypeId("aoc/input.cau", "NeedInput") -> {
+                        val path = signal.values[0] as RuntimeValue.Text
+                        val inputFile = getInputFile(path.value)
+                        val text = inputFile.readText()
+
+                        RuntimeValue.Text(text)
+                    }
+
+                    vm.codeBundle.getTypeId("aoc/input.cau", "NeedInputLines") -> {
+                        val path = signal.values[0] as RuntimeValue.Text
+                        val inputFile = rootDir.resolve("project/puzzles").resolve(path.value).toFile()
+                        val lines = inputFile.readLines()
+
+                        lines.map { RuntimeValue.Text(it) }.toCauseStack(vm)
+                    }
+
+                    vm.codeBundle.getTypeId("aoc/input.cau", "ParseNumber") -> {
+                        val text = signal.values[0] as RuntimeValue.Text
+                        val parsed = text.value.toDouble()
+                        RuntimeValue.Number(parsed)
+                    }
+
+                    vm.codeBundle.getTypeId("aoc/input.cau", "Split") -> {
+                        val text = signal.values[0] as RuntimeValue.Text
+                        val separator = signal.values[1] as RuntimeValue.Text
+                        val result = text.value.split(separator.value)
+
+                        result.map { RuntimeValue.Text(it) }.toCauseStack(vm)
+                    }
+
+                    vm.codeBundle.getTypeId("aoc/interface.cau", "ReportProgress") -> {
+                        val message = signal.values[0] as RuntimeValue.Text
+
+                        println(message.value)
+                        vm.reportTick()
+                        RuntimeValue.Action
+                    }
+
+                    vm.codeBundle.getTypeId("aoc/draw.cau", "Draw") -> {
+                        val drawable = signal.values[0].validate() as RuntimeValue.RuntimeObject
+                        drawQueue.add(drawable)
+                        RuntimeValue.Action
+                    }
+
+                    vm.codeBundle.getTypeId("aoc/draw.cau", "FrameDone") -> {
+                        window.drawables = drawQueue.toList()
+                        drawQueue.clear()
+                        RuntimeValue.Action
+                    }
+
+                    else -> {
+                        throw Error("Unrecognized signal: ${signal.typeDescriptor.id}")
+                    }
                 }
 
-                vm.codeBundle.getBuiltinTypeId("AssumptionBroken") -> {
-                    val value = signal.values[0] as RuntimeValue.Text
-                    println(
-                        Ansi.ansi().fg(Ansi.Color.RED).render(vm.getExecutionTrace())
-                            .render("Assumption broken: " + value.value).reset()
-                    )
-                    break
-                }
-
-                vm.codeBundle.getTypeId("aoc/input.cau", "NeedInput") -> {
-                    val path = signal.values[0] as RuntimeValue.Text
-                    val inputFile = getInputFile(path.value)
-                    val text = inputFile.readText()
-
-                    RuntimeValue.Text(text)
-                }
-
-                vm.codeBundle.getTypeId("aoc/input.cau", "NeedInputLines") -> {
-                    val path = signal.values[0] as RuntimeValue.Text
-                    val inputFile = rootDir.resolve("project/puzzles").resolve(path.value).toFile()
-                    val lines = inputFile.readLines()
-
-                    lines.map { RuntimeValue.Text(it) }.toCauseStack(vm)
-                }
-
-                vm.codeBundle.getTypeId("aoc/input.cau", "ParseNumber") -> {
-                    val text = signal.values[0] as RuntimeValue.Text
-                    val parsed = text.value.toDouble()
-                    RuntimeValue.Number(parsed)
-                }
-
-                vm.codeBundle.getTypeId("aoc/input.cau", "Split") -> {
-                    val text = signal.values[0] as RuntimeValue.Text
-                    val separator = signal.values[1] as RuntimeValue.Text
-                    val result = text.value.split(separator.value)
-
-                    result.map { RuntimeValue.Text(it) }.toCauseStack(vm)
-                }
-
-                vm.codeBundle.getTypeId("aoc/interface.cau", "ReportProgress") -> {
-                    val message = signal.values[0] as RuntimeValue.Text
-
-                    println(message.value)
-                    vm.reportTick()
-                    RuntimeValue.Action
-                }
-
-                vm.codeBundle.getTypeId("aoc/draw.cau", "Draw") -> {
-                    val drawable = signal.values[0].validate() as RuntimeValue.RuntimeObject
-                    drawQueue.add(drawable)
-                    RuntimeValue.Action
-                }
-
-                vm.codeBundle.getTypeId("aoc/draw.cau", "FrameDone") -> {
-                    window.drawables = drawQueue.toList()
-                    drawQueue.clear()
-                    RuntimeValue.Action
-                }
-
-                else -> {
-                    throw Error(vm.getExecutionTrace() + "Unrecognized signal: ${signal.typeDescriptor.id}")
-                }
+                executionState = vm.resumeExecution(resultValue)
             }
-
-            executionState = vm.resumeExecution(resultValue)
+            (executionState as? RunResult.Returned)?.returnValue
+        } catch (err: Throwable) {
+            println(
+                Ansi.ansi().fg(Ansi.Color.RED).render(vm.getExecutionTrace())
+                    .render(err.stackTraceToString()).reset()
+            )
+            null
         }
-        val returnVal = (executionState as? RunResult.Returned)?.returnValue
 
         val endTime = System.nanoTime()
 
