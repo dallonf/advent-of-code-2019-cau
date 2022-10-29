b477c361f105ca6cec4fe3c2ce52f82ca302c142
Dallon Feldner <dallon.feldner@gmail.com>
2022-10-29 06:12:33 -0500

    Setup rendering for Day 1

--- src/main/kotlin/VizWindow.kt
+++ src/main/kotlin/VizWindow.kt
@@ -1,16 +1,17 @@
 import Utils.getTypeName
-import com.dallonf.ktcause.LangVm
 import com.dallonf.ktcause.RuntimeValue
 import java.awt.*
 import javax.swing.JFrame
+import javax.swing.JPanel
 import kotlin.math.roundToInt
 
 
 class VizWindow : JFrame("Advent of Code 2019") {
-    var drawables: List<RuntimeValue.RuntimeObject> = emptyList()
+    private val drawPanel = DrawPanel()
+    var drawables: List<RuntimeValue.RuntimeObject>
+        get() = drawPanel.drawables
         set(value) {
-            field = value
-            repaint()
+            drawPanel.drawables = value
         }
 
     enum class PaletteColor(val color: Color) {
@@ -36,74 +37,90 @@ class VizWindow : JFrame("Advent of Code 2019") {
 
     init {
         defaultCloseOperation = EXIT_ON_CLOSE
-        size = Dimension(1366, 768)
-        background = PaletteColor.WHITE.color
+        isResizable = false
+        add(drawPanel)
+        pack()
 
         font = Font.createFont(Font.TRUETYPE_FONT, javaClass.getResourceAsStream("ShareTechMono-Regular.ttf"))
     }
 
-    fun open() {
-        isVisible = true
-    }
-
-    override fun paint(g: Graphics?) {
-        super.paint(g)
-        val g2d = g as? Graphics2D ?: return
-        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
-        g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON)
-        g2d.clearRect(0, 0, width, height)
-
-        for (drawable in drawables) {
-            when (getTypeName(drawable)) {
-                "Rectangle" -> paintRectangle(g2d, drawable)
-                "DrawableText" -> paintText(g2d, drawable)
+    class DrawPanel : JPanel() {
+        var drawables: List<RuntimeValue.RuntimeObject> = emptyList()
+            set(value) {
+                field = value
+                repaint()
             }
+
+        init {
+            preferredSize = Dimension(1366, 768)
+            background = PaletteColor.WHITE.color
+        }
+
+        override fun paint(g: Graphics?) {
+            super.paint(g)
+            val g2d = g as? Graphics2D ?: return
+            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
+            g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON)
+
+            for (drawable in drawables) {
+                when (getTypeName(drawable)) {
+                    "Rectangle" -> paintRectangle(g2d, drawable)
+                    "DrawableText" -> paintText(g2d, drawable)
+                }
+            }
+        }
+
+        private fun paintRectangle(g2d: Graphics2D, drawable: RuntimeValue.RuntimeObject) {
+            val color = PaletteColor.fromCause(drawable.values[0])
+            val x = (drawable.values[1] as RuntimeValue.Number).asDouble()
+            val y = (drawable.values[2] as RuntimeValue.Number).asDouble()
+            val width = (drawable.values[3] as RuntimeValue.Number).asDouble()
+            val height = (drawable.values[4] as RuntimeValue.Number).asDouble()
+
+            g2d.color = color.color
+            g2d.fillRect(x.roundToInt(), y.roundToInt(), width.roundToInt(), height.roundToInt())
+        }
+
+        private fun paintText(g2d: Graphics2D, drawable: RuntimeValue.RuntimeObject) {
+            val values = drawable.values.iterator()
+            val text = (values.next() as RuntimeValue.Text).value
+            val color = PaletteColor.fromCause(values.next())
+            val fontSize = (values.next() as RuntimeValue.Number).asDouble()
+            val x = (values.next() as RuntimeValue.Number).asDouble()
+            val y = (values.next() as RuntimeValue.Number).asDouble()
+            val vAlignment = getTypeName(values.next())
+            val hAlignment = getTypeName(values.next())
+
+            val prevFont = font
+
+            g2d.font = font.deriveFont(fontSize.toFloat())
+            val textWidth = g2d.fontMetrics.stringWidth(text)
+            val textHeight = g2d.fontMetrics.height
+
+            val xAligned: Float = when (hAlignment) {
+                "Left" -> x
+                "Center" -> x - textWidth / 2
+                "Right" -> x - textWidth
+                else -> throw AssertionError("bad alignment: $hAlignment")
+            }.toFloat()
+
+            val yAligned: Float = when (vAlignment) {
+                "Top" -> x + textHeight
+                "Center" -> x + textHeight / 2
+                "Bottom" -> y
+                else -> throw AssertionError("bad alignment: $vAlignment")
+            }.toFloat()
+
+            g2d.color = color.color
+            g2d.drawString(text, xAligned, yAligned)
+            g2d.font = prevFont
         }
     }
 
-
-    private fun paintRectangle(g2d: Graphics2D, drawable: RuntimeValue.RuntimeObject) {
-        val color = PaletteColor.fromCause(drawable.values[0])
-        val x = (drawable.values[1] as RuntimeValue.Number).asDouble()
-        val y = (drawable.values[2] as RuntimeValue.Number).asDouble()
-        val width = (drawable.values[3] as RuntimeValue.Number).asDouble()
-        val height = (drawable.values[4] as RuntimeValue.Number).asDouble()
-
-        g2d.color = color.color
-        g2d.fillRect(x.roundToInt(), y.roundToInt(), width.roundToInt(), height.roundToInt())
+    fun open() {
+        isVisible = true
+        size = Dimension(1366 + insets.left + insets.right, 768 + insets.top + insets.bottom)
     }
 
-    private fun paintText(g2d: Graphics2D, drawable: RuntimeValue.RuntimeObject) {
-        val text = (drawable.values[0] as RuntimeValue.Text).value
-        val color = PaletteColor.fromCause(drawable.values[1])
-        val x = (drawable.values[2] as RuntimeValue.Number).asDouble()
-        val y = (drawable.values[3] as RuntimeValue.Number).asDouble()
-        val fontSize = (drawable.values[4] as RuntimeValue.Number).asDouble()
-        val hAlignment = getTypeName(drawable.values[5])
-        val vAlignment = getTypeName(drawable.values[6])
 
-        val prevFont = font
-
-        g2d.font = font.deriveFont(fontSize.toFloat())
-        val textWidth = g2d.fontMetrics.stringWidth(text)
-        val textHeight = g2d.fontMetrics.height
-
-        val xAligned: Float = when (hAlignment) {
-            "Left" -> x
-            "Center" -> x - textWidth / 2
-            "Right" -> x - textWidth
-            else -> throw AssertionError("bad alignment: $hAlignment")
-        }.toFloat()
-
-        val yAligned: Float = when (vAlignment) {
-            "Top" -> x - textHeight
-            "Center" -> x - textHeight / 2
-            "Bottom" -> y
-            else -> throw AssertionError("bad alignment: $hAlignment")
-        }.toFloat()
-
-        g2d.color = color.color
-        g2d.drawString(text, xAligned, yAligned)
-        g2d.font = prevFont
-    }
 }
